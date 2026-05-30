32e22e5e3278ce00c9706398457a89ca1d477d75
Vivek <codeeradevelopers@gmail.com>
2026-05-30 12:43:45 +0530

    chnages

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,27 +1,48 @@
 @extends('layouts.app')
-
 @section('title', 'Blog')
-
 @section('content')
 
-<section class="hero-wrap hero-wrap-2 js-fullheight"
-         style="background-image: url('{{ asset('images/bg_2.jpg') }}');"
-         data-stellar-background-ratio="0.5">
+@php
+$heroBackground = site_setting('hero_background_image')
+? asset('uploads/settings/' . site_setting('hero_background_image'))
+: asset('images/bg_1.jpg');
+@endphp
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('{{ $heroBackground }}');">
   <div class="overlay"></div>
   <div class="container">
-    <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+    <div class="row no-gutters slider-text align-items-end justify-content-start">
       <div class="col-md-9 ftco-animate pb-5">
         <p class="breadcrumbs">
           <span class="mr-2">
             <a href="{{ url('/') }}">Home <i class="ion-ios-arrow-forward"></i></a>
           </span>
-          <span>Blog <i class="ion-ios-arrow-forward"></i></span>
+          <span class="mr-2">
+            <a href="{{ route('services') }}">Blogs <i class="ion-ios-arrow-forward"></i></a>
+          </span>
         </p>
-        <h1 class="mb-3 bread">Read our blog</h1>
+        <h1 class="mb-3 bread">Read our blogs</h1>
       </div>
     </div>
   </div>
 </section>
+@push('styles')
+<style>
+  .slider-text .bread:after {
+    display: none !important;
+    content: none !important;
+  }
+
+  .slider-text .bread {
+    padding-left: 0;
+  }
+
+  .hero-wrap.hero-wrap-2,
+  .hero-wrap.hero-wrap-2 .slider-text {
+    height: 400px !important;
+  }
+</style>
+@endpush
 
 <section class="ftco-section">
   <div class="container">
@@ -33,8 +54,8 @@
 
           <!-- Image -->
           <a href="{{ route('blog.single', $blog->slug) }}"
-             class="block-20 img"
-             style="background-image: url('{{ asset('uploads/blogs/'.$blog->image) }}');">
+            class="block-20 img"
+            style="background-image: url('{{ asset('uploads/blogs/'.$blog->image) }}');">
           </a>
 
           <div class="text pt-4">
@@ -72,9 +93,9 @@
         </div>
       </div>
       @empty
-        <div class="col-12 text-center">
-          <p>No blogs found.</p>
-        </div>
+      <div class="col-12 text-center">
+        <p>No blogs found.</p>
+      </div>
       @endforelse
 
     </div>
--- resources/views/happy-clients.blade.php
+++ resources/views/happy-clients.blade.php
@@ -1,28 +1,45 @@
 @extends('layouts.app')
-
-@section('title', 'happy-clients.')
-
+@section('title', 'Happy Clients')
 @section('content')
+@php
+$heroBackground = site_setting('hero_background_image')
+? asset('uploads/settings/' . site_setting('hero_background_image'))
+: asset('images/bg_1.jpg');
+@endphp
 
-
-  <!-- HERO -->
-  <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_2.jpg');">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row no-gutters slider-text js-fullheight align-items-end">
-        <div class="col-md-9 ftco-animate pb-5">
-          <p class="breadcrumbs">
-            <span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span>
-            <span>Happy Clients <i class="ion-ios-arrow-forward"></i></span>
-          </p>
-          <h1 class="mb-3 bread">Happy Clients</h1>
-        </div>
+<section class="hero-wrap hero-wrap-2" style="background-image: url('{{ $heroBackground }}');">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row no-gutters slider-text align-items-end justify-content-start">
+      <div class="col-md-9 ftco-animate pb-5">
+        <p class="breadcrumbs">
+          <span class="mr-2">
+            <a href="{{ url('/') }}">Home <i class="ion-ios-arrow-forward"></i></a>
+          </span>
+          <span class="mr-2">
+            <a href="{{ route('services') }}">Happy Clients <i class="ion-ios-arrow-forward"></i></a>
+          </span>
+        </p>
       </div>
     </div>
-  </section>
+  </div>
+</section>
+@push('styles')
+<style>
+  .slider-text .bread:after {
+    display: none !important;
+    content: none !important;
+  }
 
- @include('sections.testimonials')
+  .slider-text .bread {
+    padding-left: 0;
+  }
 
-
-
+  .hero-wrap.hero-wrap-2,
+  .hero-wrap.hero-wrap-2 .slider-text {
+    height: 400px !important;
+  }
+</style>
+@endpush
+@include('sections.testimonials')
 @endsection
