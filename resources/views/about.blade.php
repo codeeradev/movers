4140353082861cd517447c20317b789b26781d9b
Vivek <codeeradevelopers@gmail.com>
2026-05-30 12:39:59 +0530

    chnages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,31 +1,49 @@
 @extends('layouts.app')
-
 @section('title', 'Home')
-
 @section('content')
 
-  <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
-          <div class="col-md-9 ftco-animate pb-5">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About us <i class="ion-ios-arrow-forward"></i></span></p>
-            <h1 class="mb-3 bread">About Us</h1>
-          </div>
-        </div>
+@php
+$heroBackground = site_setting('hero_background_image')
+? asset('uploads/settings/' . site_setting('hero_background_image'))
+: asset('images/bg_1.jpg');
+@endphp
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('{{ $heroBackground }}');">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row no-gutters slider-text align-items-end justify-content-start">
+      <div class="col-md-9 ftco-animate pb-5">
+        <p class="breadcrumbs">
+          <span class="mr-2">
+            <a href="{{ url('/') }}">Home <i class="ion-ios-arrow-forward"></i></a>
+          </span>
+          <span class="mr-2">
+            <a href="{{ route('services') }}">About <i class="ion-ios-arrow-forward"></i></a>
+          </span>
+        </p>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
+@push('styles')
+<style>
+  .slider-text .bread:after {
+    display: none !important;
+    content: none !important;
+  }
 
-   @include('sections.services')
+  .slider-text .bread {
+    padding-left: 0;
+  }
 
-
-     @include('sections.work')
- @include('sections.testimonials')
-
- @include('sections.about-us')
-	
-
-   
-
-
+  .hero-wrap.hero-wrap-2,
+  .hero-wrap.hero-wrap-2 .slider-text {
+    height: 400px !important;
+  }
+</style>
+@endpush
+<!-- @include('sections.services') -->
+<!-- @include('sections.work') -->
+@include('sections.about-us')
+<!-- @include('sections.testimonials') -->
 @endsection
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,43 +1,45 @@
 @extends('layouts.app')
-
 @section('title', 'Services')
-
 @section('content')
-<section class="hero-wrap hero-wrap-2" style="background-image: url('{{ asset('images/bg_2.jpg') }}');">
+@php
+$heroBackground = site_setting('hero_background_image')
+? asset('uploads/settings/' . site_setting('hero_background_image'))
+: asset('images/bg_1.jpg');
+@endphp
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('{{ $heroBackground }}');">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text align-items-end justify-content-start">
             <div class="col-md-9 ftco-animate pb-5">
-                <h1 class="mb-3 bread">Our Services</h1>
+                <p class="breadcrumbs">
+                    <span class="mr-2">
+                        <a href="{{ url('/') }}">Home <i class="ion-ios-arrow-forward"></i></a>
+                    </span>
+                    <span class="mr-2">
+                        <a href="{{ route('services') }}">Services <i class="ion-ios-arrow-forward"></i></a>
+                    </span>
+                </p>
             </div>
         </div>
     </div>
 </section>
+@push('styles')
+<style>
+    .slider-text .bread:after {
+        display: none !important;
+        content: none !important;
+    }
 
-<section class="ftco-section">
-    <div class="container">
-        <div class="row">
-            @forelse($services as $service)
-                <div class="col-md-4 d-flex ftco-animate mb-4">
-                    <div class="blog-entry justify-content-end w-100">
-                        <a href="{{ route('services.single', $service->slug) }}"
-                           class="block-20"
-                           style="background-image: url('{{ $service->image ? asset('uploads/services/'.$service->image) : asset('images/bg_1.jpg') }}');">
-                        </a>
-                        <div class="text pt-4">
-                            <h3 class="heading">
-                                <a href="{{ route('services.single', $service->slug) }}">{{ $service->title }}</a>
-                            </h3>
-                            <p>{!! Str::limit(strip_tags($service->description), 160) !!}</p>
-                        </div>
-                    </div>
-                </div>
-            @empty
-                <div class="col-12 text-center">
-                    <p>No services available yet.</p>
-                </div>
-            @endforelse
-        </div>
-    </div>
-</section>
+    .slider-text .bread {
+        padding-left: 0;
+    }
+
+    .hero-wrap.hero-wrap-2,
+    .hero-wrap.hero-wrap-2 .slider-text {
+        height: 400px !important;
+    }
+</style>
+@endpush
+@include('sections.services')
 @endsection
